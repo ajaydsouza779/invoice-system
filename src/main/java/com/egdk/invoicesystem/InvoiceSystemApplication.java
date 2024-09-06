2c965b1b987fb6a69b2db4786ffe1a49af359455
Ajay Dsouza <aj.smartbusiness@gmail.com>
2024-09-06 21:47:04 +0530

    added required dependencies

---
+++ src/main/java/com/egdk/invoicesystem/InvoiceSystemApplication.java
@@ -0,0 +1,13 @@
+package com.egdk.invoicesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoiceSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/egdk/invoicesystem/InvoiceSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.egdk.invoicesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
