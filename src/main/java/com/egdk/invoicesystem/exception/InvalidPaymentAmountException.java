5b9e84bf1bbc02a222a0f492f8e891707d870b91
Ajay Dsouza <aj.smartbusiness@gmail.com>
2024-09-09 01:32:07 +0530

    added logging

--- src/main/java/com/egdk/invoicesystem/exception/InvalidPaymentAmountException.java
+++ src/main/java/com/egdk/invoicesystem/exception/InvalidPaymentAmountException.java
@@ -1,11 +1,15 @@
 package com.egdk.invoicesystem.exception;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.math.BigDecimal;
 
 import static com.egdk.invoicesystem.constants.Messages.INVALID_PAYMENT_AMOUNT;
 
+@Slf4j
 public class InvalidPaymentAmountException extends RuntimeException {
     public InvalidPaymentAmountException(BigDecimal amount) {
         super(INVALID_PAYMENT_AMOUNT + amount);
+        log.error(INVALID_PAYMENT_AMOUNT+" {}", amount);
     }
 }
--- src/main/java/com/egdk/invoicesystem/exception/InvoiceAlreadyPaidException.java
+++ src/main/java/com/egdk/invoicesystem/exception/InvoiceAlreadyPaidException.java
@@ -1,10 +1,14 @@
 package com.egdk.invoicesystem.exception;
 
 
+import lombok.extern.slf4j.Slf4j;
+
 import static com.egdk.invoicesystem.constants.Messages.INVOICE_ALREADY_PAID;
 
+@Slf4j
 public class InvoiceAlreadyPaidException extends RuntimeException {
-    public InvoiceAlreadyPaidException() {
+    public InvoiceAlreadyPaidException(Long id) {
         super(INVOICE_ALREADY_PAID);
+        log.error("Invoice with id {} is already marked paid in previous transactions", id);
     }
 }
